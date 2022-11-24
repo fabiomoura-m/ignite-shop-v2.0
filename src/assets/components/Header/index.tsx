836936cd5a48926085666fe7424aa8f19d34ea5f
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2022-11-24 15:51:09 -0300

    Ajuste na pagina de success para mostrar todos produtos
    Alterado no header o Cart para ser exibido apenas em paginas diferentes da success

--- src/assets/components/Header/index.tsx
+++ src/assets/components/Header/index.tsx
@@ -1,16 +1,21 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import logoImg from '../../logo.svg';
 import { Cart } from '../Cart';
 import { HeaderContainer } from './styles';
 
 export function Header() {
+    const { pathname } = useRouter();
+
+    const showCartButton = pathname !== '/success';
+
     return (
         <HeaderContainer>
             <Link href="/">
                 <Image src={logoImg} alt="" />
             </Link>
-            <Cart />
+            {showCartButton && <Cart />}
         </HeaderContainer>
     );
 }
